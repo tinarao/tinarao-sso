2138f0731d44a4d5c7934bbb00e7839a5da4b96a
tinarao <golovindanila1613@gmail.com>
2024-10-06 18:38:50 +0300

    initial commit
    added some UI stuff, added prisma and configured db

---
+++ src/lib/components/ui/button.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let variant: 'danger' | 'default' = 'default';
+
+	const colors = {
+		danger: 'bg-red-500 hover:bg-red-600',
+		default: 'bg-violet-500 hover:bg-violet-600'
+	};
+</script>
+
+<button class="{colors[variant]} transition px-6 py-2 rounded-md text-white text-lg font-medium">
+	<slot />
+</button>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<div class="h-screen flex flex-col items-center justify-center space-x-4">
+	<h1 class="text-6xl font-bold">🛠️ Work in progress</h1>
+</div>
